9c424e0de092f019d99c3f032b38014ae0624c1c
Vitor Riboli <vitorriboli1@gmail.com>
2022-05-30 19:53:38 -0300

    teste

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+
+var numeroSecreto = parseInt(Math.random() * 11); //Se esse codigo ficar fora do escopo ele sera gerado apenas uma vez
+console.log(numeroSecreto)
 function Chutar(){
-    var numeroSecreto = parseInt(Math.random() * 11);
+   // var numeroSecreto = parseInt(Math.random() * 11); // aqui dentro toda vez que clicar no botao será gerado um novo numero
     var resultado = document.getElementById("resultado");
+    var dica = document.getElementById("dica");
     var chute = parseInt(document.getElementById("valor").value);
     
     if(chute == numeroSecreto){
@@ -9,6 +13,14 @@ function Chutar(){
         resultado.innerHTML = "Você deve digitar um número de 0 a 10";
     } else {
         resultado.innerHTML = "Você Errou";
+    }
+
+    if (chute == numeroSecreto){
+        dica.innerHTML = "Parabéns"
+    } else if(numeroSecreto >= 0 && numeroSecreto <= 5){
+        dica.innerHTML = "Numero entre 0 e 5"
+    } else if (numeroSecreto >= 6 && numeroSecreto <= 10){
+        dica.innerHTML = "Número entre 6 e 10"
     }
 };
 
